960838101c6e78e407872841b3c8b7d69ff290f2
chibaf <chibaf@mac.com>
2024-09-03 20:28:10 +0900

    Add files via upload

--- verlin.m
+++ verlin.m
@@ -1,3 +1,8 @@
+%usage:
+% setround(-1): round toward downward
+% setround(0): round to nearest
+% setround(1): round toward upward
+% setround(2): round Round toward zero
 clear
 N=20
 A=rand(N);
@@ -5,10 +10,10 @@ b=rand(N,1);
 R=inv(A);
 x=R*b;
 solution=x
-% down
+setround(-1); % down
 Gd=abs(R*A-eye(N));
 rd=A*x-b;
-% up
+setround(1); % up
 Gu=abs(R*A-eye(N));
 ru=A*x-b;
 center=(ru+rd)/2;
@@ -16,13 +21,13 @@ radius=center-rd;
 ru=max(rd,ru);
 Gu=max(Gd,Gu);
 R_norm=norm(R,inf);
-G_norm=borm(Gu,inf);
+G_norm=norm(Gu,inf);
 Rru=abs(R*center+abs(R)*radius);
-% down
+setround(-1); % down
 Rrd=abs(R*center+abs(R)*radius)
 D=1-G_norm;
 if D>0
-%  up
+ setround(1); %  up
  Rru=max(Rrd,Rru);
  Rr_norm=norm(Rru,inf);
  error=Rr_norm/D;
